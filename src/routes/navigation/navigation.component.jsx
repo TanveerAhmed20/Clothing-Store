b000d711e26cb693076ff2c5cccfc4a9de33d5f5
tanveerahmed20 <tanveer.ahmed@brillio.com>
2022-07-23 15:01:50 +0530

    completed till section 5

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,19 +1,25 @@
 import { Link, Outlet } from "react-router-dom";
 import React from "react";
+import { ReactComponent as Crwn } from "../../assets/crown.svg";
+// import {Fragment} from 'react'  this also works
+import './navigation.styles.scss'
 export default function Navigation() {
   return (
     <React.Fragment>
       <div className="navigation">
         <div>
-            <Link className="logo-link" to='/'>LOGO</Link>
+          <Link className="logo-container" to="/">
+            <Crwn className="logo" />
+          </Link>
         </div>
-        <div>Logo</div>
-        <div className ="links-container">
-            <Link className ="nav-link" to ='/shop'>SHOP</Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
         </div>
-        <div> This is Navigation Bar</div>
-        <Outlet />
+       
       </div>
+      <Outlet />
     </React.Fragment>
   );
 }
